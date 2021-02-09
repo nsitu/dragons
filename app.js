b75e0d82c435a470b2c3049e656501786557bdbf
Harold Sikkema <harold@nsitu.ca>
2021-02-09 00:43:34 -0500

    app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+const dotenv = require('dotenv').config()
+const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+
+const app	= express();  			// activate an express app
+app.use( express.json() ); 		// enable parsing of JSON data in our app
+
+app.use('/DragonAPI', express.static('public'))
+
+// Listen for HTTP GET requests.
+app.get('/DragonAPI/dragons/', (req, res) => {
+
+  const client = new MongoClient(
+        process.env.DB, {
+          useUnifiedTopology: true,
+          useNewUrlParser: true
+        }
+    );
+
+  client.connect(err => {
+    if (err) { console.log(err); }
+    let query = {height:{$gt:10}}
+    let projection = {'name':1,'weight':1,'height':1, 'sprites':1,'_id':0}
+    // search mongo collection for all dragons
+    client.db("Dragons").collection("Pokemon")
+      .find( query )
+      .project( projection )
+      .toArray( (err, item) => {
+        if (err) { res.send({ 'error': 'An error has occured' }); }
+        else { res.send( item );	}		// send the result back.
+      });
+  });
+});
+
+  app.listen(0, () => {
+   console.log("We are live." );
+ });
